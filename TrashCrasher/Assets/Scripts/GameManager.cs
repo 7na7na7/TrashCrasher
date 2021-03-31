f8a8d1ce352f52465b79d0075efa166521c406f2
SexyM <48942066+SexyMg@users.noreply.github.com>
2021-03-31 09:49:27 +0900

    UI, 변수선언

--- TrashCrasher/Assets/Scripts/GameManager.cs
+++ TrashCrasher/Assets/Scripts/GameManager.cs
@@ -1,12 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private string tireKey = "tireKey";
+    public Image tire;
+    public Image[] tires;
+    private string engineKey = "engineKey";
+    public Image engine;
+    public Image[] engines;
+    private string oilKey = "oilKey";
+    public Image oil;
+    public Image[] oils;
+    private string bumperKey = "bumperKey";
+    public Image bumper;
+    public Image[] bumpers;
+    private string trashPullerKey = "trashPullerKey";
+    public Image trashPuller;
+    public Image[] trashPullers;
+    private string timingBelKey = "timingBeltKey";
+    public Image timingBelt;
+    public Image[] timingBelts;
+    
+    
     public GameObject GameoverPanel;
     public GameObject Car;
+    
+
     public void StartGame()
     {
         GameoverPanel.SetActive(false);
